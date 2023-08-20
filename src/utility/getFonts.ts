f413bd8e659eb371499cb231c96f8832df11a77a
Alan Norbauer <altano@gmail.com>
2023-08-20 13:24:47 -0700

    Make opengraph-image footer bolder

--- src/utility/getFonts.ts
+++ src/utility/getFonts.ts
@@ -1,17 +1,23 @@
 import type { Font } from "satori";
 
 export default async function getFonts(): Promise<Font[]> {
-  const [interRegular, interSemiBold, interBold] = await Promise.all([
-    fetch(`https://rsms.me/inter/font-files/Inter-Regular.woff`).then((res) =>
-      res.arrayBuffer()
-    ),
-    fetch(`https://rsms.me/inter/font-files/Inter-SemiBold.woff`).then((res) =>
-      res.arrayBuffer()
-    ),
-    fetch(`https://rsms.me/inter/font-files/Inter-Bold.woff`).then((res) =>
-      res.arrayBuffer()
-    ),
-  ]);
+  // This is unfortunate but I can't figure out how to load local font files
+  // when deployed to vercel.
+  const [interRegular, interMedium, interSemiBold, interBold] =
+    await Promise.all([
+      fetch(`https://rsms.me/inter/font-files/Inter-Regular.woff`).then((res) =>
+        res.arrayBuffer()
+      ),
+      fetch(`https://rsms.me/inter/font-files/Inter-Medium.woff`).then((res) =>
+        res.arrayBuffer()
+      ),
+      fetch(`https://rsms.me/inter/font-files/Inter-SemiBold.woff`).then(
+        (res) => res.arrayBuffer()
+      ),
+      fetch(`https://rsms.me/inter/font-files/Inter-Bold.woff`).then((res) =>
+        res.arrayBuffer()
+      ),
+    ]);
 
   return [
     {
@@ -19,6 +25,12 @@ export default async function getFonts(): Promise<Font[]> {
       data: interRegular,
       style: "normal",
       weight: 400,
+    },
+    {
+      name: "Inter",
+      data: interMedium,
+      style: "normal",
+      weight: 500,
     },
     {
       name: "Inter",
