d067b0ba60d0a7099eb7c5dc30fce188a5066cef
Alan Norbauer <altano@gmail.com>
2023-08-22 19:48:19 -0700

    homepage: columns

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -2,7 +2,12 @@ import { styled } from "@styled-system/jsx";
 
 export const Cards = styled("ul", {
   base: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
     padding: "0",
+    columnGap: "4rem",
+    rowGap: "1rem",
     listStyleType: "none",
     marginBlock: "2",
     "& h3": {
@@ -15,6 +20,7 @@ export const Card = styled("li", {
   base: {
     display: "flex",
     flexDirection: "column",
+    width: "calc(50% - 2rem)",
     gap: 2,
     marginBlockStart: 4,
     marginBlockEnd: 10,
