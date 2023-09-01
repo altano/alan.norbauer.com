ec7811a009a8bb48cccbcb33f763a6613d8bb4e8
Alan Norbauer <altano@gmail.com>
2023-09-01 15:22:40 -0700

    homepage: fix on mobile

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -3,11 +3,14 @@ import { styled } from "@styled-system/jsx";
 export const Cards = styled("ul", {
   base: {
     display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: "column",
+    md: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      columnGap: "4rem",
+      rowGap: "1rem",
+    },
     padding: "0",
-    columnGap: "4rem",
-    rowGap: "1rem",
     listStyleType: "none",
     marginBlock: "2",
     "& h3": {
@@ -20,7 +23,9 @@ export const Card = styled("li", {
   base: {
     display: "flex",
     flexDirection: "column",
-    width: "calc(50% - 2rem)",
+    md: {
+      width: "calc(50% - 2rem)",
+    },
     gap: 2,
     marginBlockStart: 4,
     marginBlockEnd: 10,
