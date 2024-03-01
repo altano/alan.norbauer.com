4bf2d00db6212a5ae1a5ed53e282f64d1ff0d93e
Alan Norbauer <altano@gmail.com>
2024-03-01 00:47:15 -0800

    opengraph-image hack, enable export

--- src/app/articles/[slug]/og.png/route.ts
+++ src/app/articles/[slug]/og.png/route.ts
@@ -1,17 +1,25 @@
+import { getArticles } from "@/content-utils/query/articles";
 import { getArticleBySlug } from "@/content-utils/query/article";
 import OpenGraphImage from "@/components/opengraph/image";
 import getFonts from "@/utility/getFonts";
+import type { ArticleProps } from "../page";
 
-import type { ArticleProps } from "./page";
-
-export const runtime = "nodejs";
-export const contentType = "image/png";
-export const size = {
+const size = {
   width: 1200,
   height: 630,
 };
 
-export default async function Image({ params }: ArticleProps) {
+export async function generateStaticParams(): Promise<
+  ArticleProps["params"][]
+> {
+  const articles = await getArticles();
+  return articles.map((a) => ({ slug: a.slug }));
+}
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { slug: string } },
+) {
   const article = await getArticleBySlug(params.slug);
   const authors = article.authors.map((a) => a.name).join(" and");
   const fonts = await getFonts();
