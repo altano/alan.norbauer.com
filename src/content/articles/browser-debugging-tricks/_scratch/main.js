9a924c76739cc00641bfb2511910144f4fbb3960
Alan Norbauer <altano@gmail.com>
2025-04-22 20:18:35 -0700

    rename scratch folder

--- src/content/articles/browser-debugging-tricks/_scratch/main.js
+++ src/content/articles/browser-debugging-tricks/_scratch/main.js
@@ -22,72 +22,66 @@
 //   location.host = 'example.com'; // Still works
 // })();
 
-;(function functionArityBreakpoint() {
+(function functionArityBreakpoint() {
   function stopOnWrongArguments(name) {
-    console.log(`Hello, ${name}`)
+    console.log(`Hello, ${name}`);
   }
 
-  stopOnWrongArguments("alan")
-  stopOnWrongArguments()
-})
-
-;(function argumentLengthBreakpoint() {
+  stopOnWrongArguments("alan");
+  stopOnWrongArguments();
+});
+(function argumentLengthBreakpoint() {
   function stopOn3Arguments(name, age, occupation = "shitty magician") {
-    console.log(`Hello, ${name}`)
+    console.log(`Hello, ${name}`);
   }
 
-  stopOn3Arguments()
-  stopOn3Arguments("alan", 34)
-  stopOn3Arguments("alan", 34, "sad magician")
-})
-
-;(function perfMeasurement() {
+  stopOn3Arguments();
+  stopOn3Arguments("alan", 34);
+  stopOn3Arguments("alan", 34, "sad magician");
+});
+(function perfMeasurement() {
   for (let i = 0; i < 100000; ++i) {
-    window.getComputedStyle(document.body)
+    window.getComputedStyle(document.body);
   }
 
-  console.log("done")
-})
-
-;(function breakEveryOtherCall() {
+  console.log("done");
+});
+(function breakEveryOtherCall() {
   for (let i = 0; i < 10; ++i) {
-    console.log(i)
+    console.log(i);
   }
-})
-
-;(function monitorClassMethodCalls() {
+});
+(function monitorClassMethodCalls() {
   function doSomethingWithADog(dog) {
-    dog.bark(2)
+    dog.bark(2);
   }
 
   class Dog {
     bark(count) {
-      console.log("woo" + "f".padEnd(count, "f"))
+      console.log("woo" + "f".padEnd(count, "f"));
     }
   }
 
-  const dog = new Dog()
-  doSomethingWithADog(dog)
-})
-
-;(function monitorInstanceMethodCalls() {
+  const dog = new Dog();
+  doSomethingWithADog(dog);
+});
+(function monitorInstanceMethodCalls() {
   function doSomethingWithADog(dog) {
-    dog.bark(2)
+    dog.bark(2);
   }
 
   class Dog {
     bark(count) {} //console.log('woo' + 'f'.padEnd(count, 'f')); }
   }
 
-  const dog = new Dog()
-  doSomethingWithADog(dog)
+  const dog = new Dog();
+  doSomethingWithADog(dog);
 
   setTimeout(() => {
-    doSomethingWithADog(dog)
-  }, 6000)
-})
-
-;(function traceMismatchedCalls() {
+    doSomethingWithADog(dog);
+  }, 6000);
+});
+(function traceMismatchedCalls() {
   function showSpinner() {
     /* ... */
   }
@@ -96,33 +90,31 @@
     /* ... */
   }
 
-  showSpinner()
+  showSpinner();
 
-  hideSpinner()
+  hideSpinner();
 
-  showSpinner() // Bad line: has no matching hideSpinner() call.
-})
-
-;(function logXHR() {
+  showSpinner(); // Bad line: has no matching hideSpinner() call.
+});
+(function logXHR() {
   function getAww(position) {
-    const url = `https://www.reddit.com/r/aww.json?limit=1&after=${position}`
-    return fetch(url)
+    const url = `https://www.reddit.com/r/aww.json?limit=1&after=${position}`;
+    return fetch(url);
   }
 
-  getAww(0).then(data => console.log(data))
-})
-
-;(function logFetches() {
+  getAww(0).then((data) => console.log(data));
+});
+(function logFetches() {
   function getData(url) {
-    return fetch(url)
+    return fetch(url);
     //return Promise.resolve({});
   }
   function getPerson(id) {
-    const url = `/person/${id}`
-    return getData(url)
+    const url = `/person/${id}`;
+    return getData(url);
   }
-  getPerson(4).then(data => console.log(data))
-})
+  getPerson(4).then((data) => console.log(data));
+});
 
 // (function createGlobal() {
 //   function isValid(i) {
