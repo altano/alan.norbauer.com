14d2088d101eebabeab8939fd8be22a7cb9de6df
Alan Norbauer <altano@gmail.com>
2025-07-06 15:23:01 -0700

    use postcss mixins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,16 @@
 const autoprefixer = require("autoprefixer");
 const customMedia = require("postcss-custom-media");
 const postcssGlobalData = require("@csstools/postcss-global-data");
+const mixins = require("postcss-mixins");
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: [
     postcssGlobalData({
       files: ["./src/styles/global/custom-media-queries.css"],
+    }),
+    mixins({
+      mixinsFiles: ["./src/styles/global/mixins.css"],
     }),
     autoprefixer,
     customMedia,
